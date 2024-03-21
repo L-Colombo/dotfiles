00ded6505aea5108ed76702a6859b97b313ed695
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2024-03-21 17:49:10 +0100

    feat:add vue to nvim-ts-autotag files list

--- .config/nvim/lua/rustego/plugins-setup.lua
+++ .config/nvim/lua/rustego/plugins-setup.lua
@@ -165,12 +165,21 @@ require("lazy").setup({
 
 
         "lervag/vimtex",
-        -- { "dgagn/diagflow.nvim",    opts = { scope = "line" }, },
         {
             "windwp/nvim-ts-autotag",
             config = function()
                 require("nvim-ts-autotag").setup({
-                    filetypes = { "html", "twig", "javascript", "javascriptreact", "typescript", "typescriptreact", "jsx", "tsx" },
+                    filetypes = {
+                        "html",
+                        "twig",
+                        "javascript",
+                        "javascriptreact",
+                        "typescript",
+                        "typescriptreact",
+                        "jsx",
+                        "tsx",
+                        "vue"
+                    },
                 })
             end
         },
