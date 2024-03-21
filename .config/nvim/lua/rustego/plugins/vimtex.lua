1b2bff80d330b47e46c1ab3c739a0e4a229e154e
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2024-03-21 15:09:32 +0100

    fix:typo in vimtex.lua

--- .config/nvim/lua/rustego/plugins/vimtex.lua
+++ .config/nvim/lua/rustego/plugins/vimtex.lua
@@ -1,7 +1,7 @@
 vim.g['vimtex_view_method'] = 'skim'
 vim.g['vimtex_quickfix_mode'] = 0
 vim.g['vimtex_view_skim_sync'] = 1
-vim.g['wimtex_view_skim_activate'] = 1
+vim.g['vimtex_view_skim_activate'] = 1
 vim.g['vimtex_mappings_enabled'] = 1
 vim.g['vimtex_indent_enabled'] = 1
 vim.g['vimtex_syntax_enabled'] = 1
