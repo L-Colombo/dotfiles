5ede460e513e5c379d6a4db61c57d16f06cbd6f5
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2024-04-24 00:03:25 +0200

    fix(nvim): set current buffer highlight for lualine

--- .config/nvim/lua/conf/core/autocommands.lua
+++ .config/nvim/lua/conf/core/autocommands.lua
@@ -1,7 +1,15 @@
+-- highlight on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- active buffer highlight in lualine
+vim.api.nvim_create_autocmd('ColorScheme', {
+  callback = function()
+    vim.cmd.highlight('lualine_c_buffers_active guifg=#3e8fb0')
+  end
 })
