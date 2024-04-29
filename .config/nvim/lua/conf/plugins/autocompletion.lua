2bc0f20d7fc6cac854a79db7871f52e232e324ce
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2024-04-29 08:24:53 +0200

    nvim(snippets): extend html snippets in jsx/tsx files

--- .config/nvim/lua/conf/plugins/autocompletion.lua
+++ .config/nvim/lua/conf/plugins/autocompletion.lua
@@ -1,5 +1,5 @@
 return {
-  "hrsh7th/nvim-cmp",   -- Autocompletion engine and snippet sources
+  "hrsh7th/nvim-cmp", -- Autocompletion engine and snippet sources
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-path",
@@ -8,12 +8,15 @@ return {
     "rafamadriz/friendly-snippets",
   },
   config = function()
-    local cmp = require('cmp')     --autocompletion
+    local cmp = require('cmp') --autocompletion
 
     require("luasnip.loaders.from_vscode").lazy_load()
-    require("luasnip.loaders.from_vscode").load({ paths = "~/.config/nvim/snippets" })     -- load custom snippets
+    require("luasnip.loaders.from_vscode").load({ paths = "~/.config/nvim/snippets" }) -- load custom snippets
     -- uncomment to have blade snippets in Laravel projects
     -- require("luasnip").filetype_extend("php", { "blade" })
+    -- uncomment to have html snippets in React projects
+    -- require("luasnip").filetype_extend("typescriptreact", { "html" })
+    -- require("luasnip").filetype_extend("javascriptreact", { "html" })
     cmp.setup({
       mapping = {
         ["<C-p>"] = cmp.mapping.select_prev_item(),
