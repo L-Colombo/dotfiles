fa032744072d10928379fafaa4117da968096eb1
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2024-09-09 08:53:28 -0400

    nvim(lsp): update from deprecated `tsserver` to `ts_ls`

--- .config/nvim/lua/conf/plugins/mason.lua
+++ .config/nvim/lua/conf/plugins/mason.lua
@@ -6,7 +6,7 @@ return {
     require("mason").setup()
     require("mason-lspconfig").setup({
       ensure_installed = {
-        'tsserver',
+        'ts_ls',
         'html',
         'cssls',
         'lua_ls',
