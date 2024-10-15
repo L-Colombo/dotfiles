87b19f2d8e860b708405ace41c85c4c9f5e27338
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2024-10-14 22:49:16 -0400

    nvim(indentmini): more muted color for indentation lines

--- .config/nvim/lua/conf/plugins/indentmini.lua
+++ .config/nvim/lua/conf/plugins/indentmini.lua
@@ -1,9 +1,9 @@
 return {
   "nvimdev/indentmini.nvim",
   config = function ()
-    vim.cmd.highlight('IndentLine guifg=#666666')
-    vim.cmd.highlight('IndentLineCurrent guifg=#666666')
     require("indentmini").setup({
+      vim.cmd.highlight('IndentLine guifg=#666666'),
+      vim.cmd.highlight('IndentLineCurrent guifg=#666666'),
       exclude = {"markdown", "tex"}
     })
   end
