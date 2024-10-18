ea6b30cec387993d124563c3a45b9398d8145797
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2024-10-18 13:57:22 -0400

    emacs(startup): make startup time shorter

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,4 @@
 (unless (file-exists-p "~/.emacs.d/custom.el")
   (shell-command "touch ~/.emacs.d/custom.el"))
-(setq custom-file "~/.emacs.d/custom.el")
-(load custom-file)
 
-(org-babel-load-file "~/.emacs.d/configuration.org")
+(org-babel-load-file "~/.emacs.d/configuration.org" nil)
