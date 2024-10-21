3d64d07d02615d40b4eaab85df56b7ad1221e2a3
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2024-10-21 10:15:22 -0400

    emacs(init): add init hooks fo further optimization

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,4 @@
 (unless (file-exists-p "~/.emacs.d/custom.el")
   (shell-command "touch ~/.emacs.d/custom.el"))
-
+(load "~/.emacs.d/custom.el")
 (org-babel-load-file "~/.emacs.d/configuration.org" nil)
