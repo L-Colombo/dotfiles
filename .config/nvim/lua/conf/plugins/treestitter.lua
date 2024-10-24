20579f498598e88da7eab4dae1267dc09327bc49
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2024-10-23 23:02:06 -0400

    nvim(treesitter): add keymaps for incremental highlighting

--- .config/nvim/lua/conf/plugins/treestitter.lua
+++ .config/nvim/lua/conf/plugins/treestitter.lua
@@ -9,7 +9,16 @@ return {
       sync_install = false,
       auto_install = true,
       highlight = { enable = true },
-      indent = { enable = true }
+      indent = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "+",
+          node_incremental = "+",
+          scope_incremental = "+",
+          node_decremental = "-"
+        }
+      }
     }
   end
 }
