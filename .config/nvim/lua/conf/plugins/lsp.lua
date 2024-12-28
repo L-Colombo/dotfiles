ca5ae8a9f2f5a5c4109cce3aff63dfcf6a9ea7c1
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2024-12-28 12:40:19 +0100

    nvim(lsp): format file

--- .config/nvim/lua/conf/plugins/lsp.lua
+++ .config/nvim/lua/conf/plugins/lsp.lua
@@ -1,60 +1,60 @@
 return {
-  "neovim/nvim-lspconfig",
-  lazy = false,
-  config = function()
-    local capabilities = require("blink.cmp").get_lsp_capabilities()
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+        local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-    local lspconfig = require("lspconfig")
-    lspconfig.bashls.setup({ -- bash
-      capabilities = capabilities
-    })
-    lspconfig.clangd.setup({ -- c/c++
-      capabilities = capabilities
-    })
-    lspconfig.gopls.setup({ -- go (**)
-      capabilities = capabilities
-    })
-    lspconfig.hls.setup({ -- haskell (*)
-      capabilities = capabilities
-    })
-    lspconfig.html.setup({ -- html
-      capabilities = capabilities
-    })
-    lspconfig.intelephense.setup({ -- php
-      capabilities = capabilities
-    })
-    lspconfig.lua_ls.setup({ -- lua
-      capabilities = capabilities,
-    })
-    lspconfig.pyright.setup({ -- python
-      capabilities = capabilities,
-      settings = {
-        pyright = {
-          disableOrganizeImports = true,
-        },
-        python = {
-          analysis = {
-            ignore = { "*" },
-          }
-        }
-      }
-    })
-    lspconfig.ruff.setup({ -- python
-      capabilities = capabilities
-    })
-    lspconfig.rust_analyzer.setup({ -- rust (*)
-      capabilities = capabilities
-    })
-    lspconfig.tailwindcss.setup({ -- tailwindcss
-      capabilities = capabilities
-    })
-    lspconfig.texlab.setup({ -- LaTeX (**)
-      capabilities = capabilities
-    })
-    lspconfig.ts_ls.setup({ -- typescript/javascript
-      capabilities = capabilities
-    })
-  end
+        local lspconfig = require("lspconfig")
+        lspconfig.bashls.setup({ -- bash
+            capabilities = capabilities
+        })
+        lspconfig.clangd.setup({ -- c/c++
+            capabilities = capabilities
+        })
+        lspconfig.gopls.setup({ -- go (**)
+            capabilities = capabilities
+        })
+        lspconfig.hls.setup({ -- haskell (*)
+            capabilities = capabilities
+        })
+        lspconfig.html.setup({ -- html
+            capabilities = capabilities
+        })
+        lspconfig.intelephense.setup({ -- php
+            capabilities = capabilities
+        })
+        lspconfig.lua_ls.setup({ -- lua
+            capabilities = capabilities,
+        })
+        lspconfig.pyright.setup({ -- python
+            capabilities = capabilities,
+            settings = {
+                pyright = {
+                    disableOrganizeImports = true,
+                },
+                python = {
+                    analysis = {
+                        ignore = { "*" },
+                    }
+                }
+            }
+        })
+        lspconfig.ruff.setup({ -- python
+            capabilities = capabilities
+        })
+        lspconfig.rust_analyzer.setup({ -- rust (*)
+            capabilities = capabilities
+        })
+        lspconfig.tailwindcss.setup({ -- tailwindcss
+            capabilities = capabilities
+        })
+        lspconfig.texlab.setup({ -- LaTeX (**)
+            capabilities = capabilities
+        })
+        lspconfig.ts_ls.setup({ -- typescript/javascript
+            capabilities = capabilities
+        })
+    end
 }
 -- (*): server installed with the toolchain
 -- (**): server installed with package manager
