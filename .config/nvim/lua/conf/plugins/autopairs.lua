a297410cfc086b5cfa1ef2d5c85af30fd6de44b5
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2025-01-13 21:40:32 +0100

    nvim(autopairs): pair angle brackets for generics

--- .config/nvim/lua/conf/plugins/autopairs.lua
+++ .config/nvim/lua/conf/plugins/autopairs.lua
@@ -3,13 +3,29 @@ return {
     event = { "insertEnter" },
     config = function()
         local autopairs = require("nvim-autopairs")
+        local Rule = require("nvim-autopairs.rule")
+        local cond = require("nvim-autopairs.conds")
         autopairs.setup({
             check_ts = true,
             ts_config = {
                 lua = { "string" },
                 javascript = { "template_string" },
-                java = false,
             },
         })
+        autopairs.add_rule(Rule('<', '>', {
+            -- if you use nvim-ts-autotag, you may want to exclude these filetypes from this rule
+            -- so that it doesn't conflict with nvim-ts-autotag
+            '-html',
+            '-javascriptreact',
+            '-typescriptreact',
+        }):with_pair(
+        -- regex will make it so that it will auto-pair on
+        -- `a<` but not `a <`
+        -- The `:?:?` part makes it also
+        -- work on Rust generics like `some_func::<T>()`
+            cond.before_regex('%a+:?:?$', 3)
+        ):with_move(function(opts)
+            return opts.char == '>'
+        end))
     end
 }
