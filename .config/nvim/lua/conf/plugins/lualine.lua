72f4e6b96f3851ddfe36c0aeb3440494715d5fc1
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2025-02-04 21:23:53 +0100

    nvim(lualine): remove ugly separators

--- .config/nvim/lua/conf/plugins/lualine.lua
+++ .config/nvim/lua/conf/plugins/lualine.lua
@@ -8,8 +8,8 @@ return {
         require("lualine").setup({
             options = {
                 theme = custom_theme,
-                component_separators = { left = '', right = '' },
-                section_separators = { left = '', right = '' },
+                component_separators = { left = "", right = "" },
+                section_separators = { left = "", right = "" },
             },
             sections = {
                 lualine_b = { { "branch" } },
