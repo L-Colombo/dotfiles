1634fdec72b0b97e71e10bb20d3d4c70c2705728
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2025-02-12 09:56:35 +0100

    nvim(conform): setup php and js/jsx

--- .config/nvim/lua/conf/plugins/conform.lua
+++ .config/nvim/lua/conf/plugins/conform.lua
@@ -6,7 +6,20 @@ return {
             formatters_by_ft = {
                 eruby = { "erb_format" },
                 haskell = { "fourmolu" },
+                javascript = {
+                    "prettierd",
+                    "prettier",
+                    command = "npx prettier . --write",
+                    stop_after_first = true
+                },
+                javascriptreact = {
+                    "prettierd",
+                    "prettier",
+                    command = "npx prettier . --write",
+                    stop_after_first = true
+                },
                 python = { "ruff" },
+                php = { "pint", lsp_format = "fallback" },
                 rust = { "rustfmt", lsp_format = "fallback" },
             },
             format_on_save = {
