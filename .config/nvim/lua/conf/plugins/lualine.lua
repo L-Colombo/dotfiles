8d21cf3698535aab563fef7be62bd9d1c4aa0c21
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2025-02-16 10:54:10 +0100

    nvim(lualine): set oldschool as theme

--- .config/nvim/lua/conf/plugins/lualine.lua
+++ .config/nvim/lua/conf/plugins/lualine.lua
@@ -3,11 +3,12 @@ return {
     "nvim-lualine/lualine.nvim",
     priority = 1000,
     config = function()
-        local custom_theme = require("lualine.themes.codedark")
-        custom_theme.normal.c.fg = "#cc0d0e"
+        -- local custom_theme = require("lualine.themes.codedark")
+        -- custom_theme.normal.c.fg = "#cc0d0e"
         require("lualine").setup({
             options = {
-                theme = custom_theme,
+                theme = "oldschool",
+                -- theme = custom_theme,
                 component_separators = { left = "", right = "" },
                 section_separators = { left = "", right = "" },
             },
