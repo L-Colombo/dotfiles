9f0f09e822e3ea2b932c1496e15e3a0bfc66d7bb
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2025-03-01 00:56:54 +0100

    nvim: set colorscheme priority

--- .config/nvim/lua/conf/plugins/colorschemes.lua
+++ .config/nvim/lua/conf/plugins/colorschemes.lua
@@ -7,6 +7,7 @@ return {
     "L-Colombo/atlantic-dark.nvim",
     {
         "L-Colombo/oldschool.nvim",
+        priority = 1000,
         opts = { blue = "#0099f8" }
     },
     -- { dir = "~/Programming/0_projects/oldschool.nvim", opts = {} }
