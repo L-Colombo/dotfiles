25f9a43141a51358592176eb19fae1ddde8cdda5
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2025-05-15 10:28:32 +0200

    nvim: colorschemes

--- .config/nvim/lua/conf/plugins/colorschemes.lua
+++ .config/nvim/lua/conf/plugins/colorschemes.lua
@@ -2,6 +2,7 @@ return {
     "oprearocks/allhallowseve.nvim",
     "kamwitsta/vinyl.nvim",
     "cpplain/flexoki.nvim",
+    "timmyha/ekkamai.nvim",
 
     -- my colorschemes
     "L-Colombo/atlantic-dark.nvim",
