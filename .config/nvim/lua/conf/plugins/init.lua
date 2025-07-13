8da2bf28e140eb8eabc67cb37cf0390e493757bf
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2025-07-13 12:22:26 +0200

    nvim: remove friendly-snippets

--- .config/nvim/lua/conf/plugins/init.lua
+++ .config/nvim/lua/conf/plugins/init.lua
@@ -2,5 +2,5 @@ return {
     -- plugins that do not require configuration
     "nvim-lua/plenary.nvim",
     "tpope/vim-surround",
-    "rafamadriz/friendly-snippets",
+    -- "rafamadriz/friendly-snippets",
 }
