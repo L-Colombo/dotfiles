e380a0010e132ce2d020e3e8b3f1409acadf67d4
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2025-09-11 09:59:36 -0400

    change font

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -58,7 +58,7 @@
 (add-to-list 'default-frame-alist '(height . 32))
 (add-to-list 'default-frame-alist '(width . 124))
 
-(set-frame-font "JetBrains Mono NL 16" nil t)
+(set-frame-font "Iosevka Nerd Font Propo 17" nil t)
 
 ;; WHITESPACE SETTINGS
 (setq-default whitespace-style '(face spaces empty tabs trailing space-mark tab-mark)
