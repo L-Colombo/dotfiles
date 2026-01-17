a1dc1729e3a7184ad3be27d50cb88caa9af7ba0c
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2026-01-17 09:36:38 -0500

    nvim(treesitter): use simpler configuration

--- .config/nvim/lua/conf/plugins/treestitter.lua
+++ .config/nvim/lua/conf/plugins/treestitter.lua
@@ -1,30 +1,5 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    build = function()
-        local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-        ts_update()
-    end,
-    config = function()
-        require("nvim-treesitter.configs").setup {
-            endwise = {
-                enable = true
-            },
-            sync_install = false,
-            auto_install = false,
-            highlight = { enable = true },
-            indent = {
-                enable = true,
-                disable = { "ocaml" }
-            },
-        }
-        local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-        parser_config.blade = {
-            install_info = {
-                url = "https://github.com/EmranMR/tree-sitter-blade",
-                files = { "src/parser.c" },
-                branch = "main",
-            },
-            filetype = "blade"
-        }
-    end
+    lazy = false,
+    build = ":TSUpdate"
 }
