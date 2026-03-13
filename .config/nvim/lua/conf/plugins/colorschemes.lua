a7def0c324ade0608775c24f0f84bcca3f88a8f6
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2026-03-13 13:03:52 -0400

    river(keymaps): add keybinding to launch Zotero

--- .config/nvim/lua/conf/plugins/colorschemes.lua
+++ .config/nvim/lua/conf/plugins/colorschemes.lua
@@ -1,10 +1,7 @@
 return {
-    "heavycircle/ronin.nvim",
     "rockerBOO/boo-colorscheme-nvim",
     "bluz71/vim-moonfly-colors",
-    "kamil-koziol/imperial.nvim",
-    "OwlfaceGames/owly.nvim",
-    "ikelaiah/nebula-drift-omega",
+    "tx3stn/nightjungle.nvim",
     -- my colorschemes
     "L-Colombo/atlantic-dark.nvim",
     "L-Colombo/devel2.nvim",
