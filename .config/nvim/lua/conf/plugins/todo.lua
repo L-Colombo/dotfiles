b746f7a8b41ff8c3f66923239945c599981a6f8b
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2026-05-03 10:51:55 -0400

    nvim(todo): add keymaps to jump to comments

--- .config/nvim/lua/conf/plugins/todo.lua
+++ .config/nvim/lua/conf/plugins/todo.lua
@@ -10,4 +10,33 @@ return {
     },
     lazy = true,
     event = { "BufEnter" },
+    vim.keymap.set("n", "[t", function()
+        require("todo-comments").jump_prev({
+            keywords = {
+                "BUG",
+                "ERROR",
+                "FIX",
+                "HACK",
+                "NOTE",
+                "PERF",
+                "TODO",
+                "WARNING"
+            }
+        })
+    end),
+
+    vim.keymap.set("n", "]t", function()
+        require("todo-comments").jump_next({
+            keywords = {
+                "BUG",
+                "ERROR",
+                "FIX",
+                "HACK",
+                "NOTE",
+                "PERF",
+                "TODO",
+                "WARNING"
+            }
+        })
+    end)
 }
