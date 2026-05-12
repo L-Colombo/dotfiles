47fa572ed63507c48497707d01db2c082e063340
Lorenzo Colombo <colombo.bb.lorenzo@gmail.com>
2026-05-12 11:38:04 -0400

    yazi: update plugins

--- .config/nvim/lua/conf/plugins/org-roam.lua
+++ .config/nvim/lua/conf/plugins/org-roam.lua
@@ -1,7 +1,7 @@
 return {
     "chipsenkbeil/org-roam.nvim",
     tag = "0.1.1",
-    dependencies = { { "nvim-orgmode/orgmode", tag = "0.3.7", } },
+    dependencies = { "nvim-orgmode/orgmode" },
     config = function()
         require("org-roam").setup({
             directory = "~/Documents/OrgFiles/org-roam",
